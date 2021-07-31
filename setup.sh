2d43153e4258d7cd7936b2db7cc1ae918ded3e70
HiroIshida <spitfire.docomo@gmail.com>
2021-08-01 03:35:57 +0900

    Initialize

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+deep_imitation_path=$(cut -d ' ' -f2 <<< $(cut -d':' -f2 <<< $(pip show deep_imitation|grep Location)))
+ln -sf $here/experimental $deep_imitation_path/deep_imitation
+
